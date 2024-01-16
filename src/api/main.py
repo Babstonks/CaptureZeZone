ccb11264be0d871ebe7e7cb9de3ccbc56039ec08
TristanGiteub <98038880+TristanGiteub@users.noreply.github.com>
2024-01-16 10:16:57 +0100

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,12 +1,48 @@
 import j2l.pytactx.agent as pytactx
+import enterzezone
+import time
 
-agent = pytactx.Agent(playerId=input("👾 id: "),
-						arena=input("🎲 arena: "),
-						username="demo",
-						password=input("🔑 password: "),
-						server="mqtt.jusdeliens.com",
-						verbosity=2)
+new_player = enterzezone.Player("joueur1", "enterzezone", "demo", "demo",
+                                "mqtt.jusdeliens.com")
+new_player1 = enterzezone.Player("joueur4", "enterzezone", "demo", "demo",
+                                 "mqtt.jusdeliens.com")
+new_player2 = enterzezone.Player("joueur3", "enterzezone", "demo", "demo",
+                                 "mqtt.jusdeliens.com")
+new_player3 = enterzezone.Player("joueur2", "enterzezone", "demo", "demo",
+                                 "mqtt.jusdeliens.com")
 
+new_player1.moveTowards(20, 25)
+time.sleep(2)
+new_player1.updateSelf()
+
+new_player2.moveTowards(20, 25)
+time.sleep(2)
+new_player2.updateSelf()
+
+new_player3.moveTowards(20, 25)
+time.sleep(2)
+new_player3.updateSelf()
+
+new_player.moveTowards(7, 21)
+time.sleep(2)
+new_player.updateSelf()
+time.sleep(2)
+new_player.moveTowards(13, 14)
+time.sleep(2)
+new_player.updateSelf()
+new_player.placeBlock()
+i = 14
+for i in range(26):
+	time.sleep(0.5)
+	new_player.moveTowards(i, 14)
+	new_player.placeBlock()
+	new_player.updateSelf()
+new_player.moveTowards(30, 14)
+time.sleep(2)
+new_player.updateSelf()
 while True:
-	agent.update()
-	agent.lookAt((agent.dir + 1) % 4)
+	new_player.moveTowards(37, 14)
+	new_player.updateSelf()
+	time.sleep(5)
+	new_player.moveTowards(38, 14)
+	new_player.updateSelf()
